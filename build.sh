2800ee38f98cae77d67d13563d67d18a278d7978
Jonathan Peppers <jonathan.peppers@hitcents.com>
2016-04-06 16:33:06 -0500

    Ok, only install fast lane now if needed

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 #fastlane setup, easier in bash
-gem install --user-install fastlane
-#if gem list -i fastlane | grep -q false; then gem install --user-install fastlane; fi
+if gem list -i fastlane | grep -q false; then gem install --user-install fastlane; fi
 export PATH=$PATH:~/.gem/ruby/2.0.0/bin/
 
 mono --runtime=v4.0 .nuget/nuget.exe install FAKE -Version 4.8.0
